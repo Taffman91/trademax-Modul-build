eafe3d0b3edcc72942370fe76fc1977324b9ff46
Taffman91 <alfredskedeback@gmail.com>
2017-03-07 00:10:43 +0100

    gnight commit

--- sketch.js
+++ sketch.js
@@ -1,7 +1,17 @@
+var data;
+
+function preload(){
+  data = loadJSON("info.json");
+}
+
 function setup() {
     createCanvas(window.innerWidth - 10, window.innerHeight - 10);
+
 }
 
 function draw(){
     background(255);
+    fill(0);
+    stroke(0);
+    rect(window.innerWidth/2, window.innerHeight/2, data.bahamas[0].width, data.bahamas[0].height);
 }
