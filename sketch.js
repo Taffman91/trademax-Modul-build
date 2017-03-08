896fa6e174e8dfd191c0e4ae62e071e2cb6458e1
Taffman91 <alfredskedeback@gmail.com>
2017-03-08 14:20:35 +0100

    Added all wisconsin and added so you can switch between series.

--- sketch.js
+++ sketch.js
@@ -1,62 +1,73 @@
 var data;
-var selectedSerieObject = 0;
+var selectedSerieObject = 1;
 var serieObjects = [];
 var serieBilder = [];
 var modulObjects = [];
-var modulBilder = [[],[]];
+var modulBilder = [
+    [],
+    []
+];
 var SerieImageSizeX = 150;
 var SerieImageSizeY = 100;
 var modulImageSizeX = 150;
 var modulImageSizeY = 100;
 
-function preload(){
-  data = loadJSON("info.json", jsonLoaded);
+function preload() {
+    data = loadJSON("info.json", jsonLoaded);
 }
 
 function setup() {
 
     createCanvas(window.innerWidth - 10, window.innerHeight - 10);
-    modulImageSizeY = (window.innerHeight-(SerieImageSizeY*2))/(data.utemobler[selectedSerieObject].data.length)-10;
-    modulImageSizeX = modulImageSizeY+(modulImageSizeY/2);
-    console.log(modulImageSizeY + " " + modulImageSizeX);
+    modulImageSizeY = (window.innerHeight - (SerieImageSizeY * 2)) / (data.utemobler[selectedSerieObject].data.length) - 10;
+    modulImageSizeX = modulImageSizeY + (modulImageSizeY / 2);
 
-    for (var i = 0; i < data.utemobler.length; i++){
-        serieObjects[i] = new serieObject(20+(i*200),20,i,serieBilder[i], data.utemobler[i].namn);
+    for (var i = 0; i < data.utemobler.length; i++) {
+        serieObjects[i] = new serieObject(20 + (i * 200), 20, i, serieBilder[i], data.utemobler[i].namn);
     }
-    for (var i = 0; i < data.utemobler[selectedSerieObject].data.length; i++){
-        modulObjects[i] = new modulObject(20,(SerieImageSizeY+40)+(i*(modulImageSizeY+20)),i,
-        modulBilder[selectedSerieObject][i],
-        data.utemobler[selectedSerieObject].data[i].name);
-        console.log((SerieImageSizeY+40)+((i*modulImageSizeY)));
+    for (var i = 0; i < data.utemobler[selectedSerieObject].data.length; i++) {
+        modulObjects[i] = new modulObject(20, (SerieImageSizeY + 40) + (i * (modulImageSizeY + 20)), i,
+            modulBilder[selectedSerieObject][i],
+            data.utemobler[selectedSerieObject].data[i].name);
     }
 }
 
-function draw(){
+function draw() {
     background(255);
-    for (var i = 0; i < serieObjects.length; i++){
-      serieObjects[i].show(selectedSerieObject);
+    for (var i = 0; i < serieObjects.length; i++) {
+        serieObjects[i].show(selectedSerieObject);
     }
-    for (var i = 0; i < modulObjects.length; i++){
-      modulObjects[i].show();
-    }
-}
-
-function mousePressed(){
-    for (var i = 0; i < serieObjects.length; i++ ){
-      var dist1 = int(dist(mouseX, mouseY, serieObjects[i].x+SerieImageSizeX/2,serieObjects[i].y+SerieImageSizeY/2));
-      if(dist1<SerieImageSizeX/2){
-        selectedSerieObject = i;
-      }
+    for (var i = 0; i < modulObjects.length; i++) {
+        modulObjects[i].show();
     }
 }
 
-function jsonLoaded(data1){
-  data = data1;
+function mousePressed() {
+    for (var i = 0; i < serieObjects.length; i++) {
+        var dist1 = int(dist(mouseX, mouseY, serieObjects[i].x + SerieImageSizeX / 2, serieObjects[i].y + SerieImageSizeY / 2));
+        if (dist1 < SerieImageSizeX / 2) {
+            if (selectedSerieObject != i) {
+                selectedSerieObject = i;
+                modulObjects = [];
+                modulImageSizeY = (window.innerHeight - (SerieImageSizeY * 2)) / (data.utemobler[selectedSerieObject].data.length) - 10;
+                modulImageSizeX = modulImageSizeY + (modulImageSizeY / 2);
+                for (var i = 0; i < data.utemobler[selectedSerieObject].data.length; i++) {
+                    modulObjects[i] = new modulObject(20, (SerieImageSizeY + 40) + (i * (modulImageSizeY + 20)), i,
+                        modulBilder[selectedSerieObject][i],
+                        data.utemobler[selectedSerieObject].data[i].name);
+                }
+            }
+        }
+    }
+}
 
-    for (var i = 0; i < data.utemobler.length; i++){
-      serieBilder[i] = loadImage(data.utemobler[i].serieimageURL);
-      for (var j = 0; j < data.utemobler[i].data.length; j++){
-        modulBilder[i][j] = loadImage(data.utemobler[i].data[j].imageURL);
-      }
+function jsonLoaded(data1) {
+    data = data1;
+
+    for (var i = 0; i < data.utemobler.length; i++) {
+        serieBilder[i] = loadImage(data.utemobler[i].serieimageURL);
+        for (var j = 0; j < data.utemobler[i].data.length; j++) {
+            modulBilder[i][j] = loadImage(data.utemobler[i].data[j].imageURL);
+        }
     }
 }
